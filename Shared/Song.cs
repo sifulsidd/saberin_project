603f5dabc86904fcf96c7df89d5559713380caad
Siful Siddiki <sifulsiddiki@yahoo.com>
2025-02-03 19:21:38 -0500

    adding ratings to Song.cs file

--- Shared/Song.cs
+++ Shared/Song.cs
@@ -13,5 +13,8 @@ namespace music_manager_starter.Shared
         public string Artist { get; set; }
         public string Album { get; set; }
         public string Genre { get; set; }
+
+        //Add a rating system
+        public int Rating { get; set; }
     }
 }
