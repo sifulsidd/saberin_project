a247cecfc80553a5e7a49592c0f2285110177ab1
Siful Siddiki <sifulsiddiki@yahoo.com>
2025-02-11 21:19:51 -0500

    Refactored and attempting new work

--- Shared/Song.cs
+++ Shared/Song.cs
@@ -14,7 +14,57 @@ namespace music_manager_starter.Shared
         public string Album { get; set; }
         public string Genre { get; set; }
 
-        //Add a rating system
-        public int Rating { get; set; }
+        ////Add a rating system, users can input ratings
+        //public int Rating { get; set; }
+
+        //// this is to determine average ratings with the total divided by number of ratings
+        //private double AverageRating;
+        //private int TotalRatings;
+        //private int NumberOfRatings;
+
+        ////checks whether or not we already rated the song
+        ////bool isRated = false;
+
+        ////equals function is there to determine if the song is the same as another song
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj is Song)
+        //    {
+        //        Song song = obj as Song;
+        //isRated = true;
+        //        return Title.Equals(song.Title) && Artist.Equals(song.Artist) && Album.Equals(song.Album) && Genre.Equals(song.Genre);
+        //    }
+
+        //    return false;
+        //}
+
+        ////rate song checks if song is rated and if it is, it will add the rating to the total ratings and increment the number of ratings
+        ////if it's not rated, we have to set the total ratings to the rating and increment the number of ratings, then find average rating
+
+        //public void RateSong(object obj)
+        //{
+        //    if (this.Equals(obj))
+        //    {
+        //        Song song = obj as Song;
+        //        TotalRatings = song.TotalRatings;
+        //        NumberOfRatings = song.NumberOfRatings;
+        //        TotalRatings += Rating;
+        //        NumberOfRatings++;
+        //        AverageRating = TotalRatings / (NumberOfRatings * 1.0);
+        //    }
+        //    else
+        //    {
+        //        TotalRatings = Rating;
+        //        NumberOfRatings = 1;
+        //        AverageRating = TotalRatings / (NumberOfRatings * 1.0);
+        //    }
+        //}
+
+        ////average rating will be displayed for each song in the song list
+        //public double GetAverageRating()
+        //{
+        //    return AverageRating;
+        //}
+
     }
 }
---
+++ music-manager-start.Data/Models/PlayList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_manager_start.Data.Models
+{
+    public sealed class PlayList
+    {
+        // created a playlist class with an id, name, and owner
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public string Owner { get; set; } = string.Empty;
+
+
+    }
+}
